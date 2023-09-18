a41f30064ce13d00ba98f0205e042ad71193a08c
Donovan Dean <donovpdean@gmail.com>
2023-09-18 12:50:13 -0700

    Initial commit for Main_Menu_Pause_Screen branch
     new branch to make improvements to the main menu and the pause screen.
    
    Trying to make it so that Menus can navigate to other scenes
    
    Also made because I have having an issue with the Menu_System branch

--- Menu_System/Scripts/main_menu.gd
+++ Menu_System/Scripts/main_menu.gd
@@ -1,11 +1,17 @@
-extends Button
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	grab_focus()
+	var quitButton = $"VBoxContainer/Quit Button"
+
+	quitButton.pressed.connect(self.on_QuitButton_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func on_QuitButton_pressed():
+	get_tree().quit()
--- Menu_System/Scripts/resume_button.gd
+++ Menu_System/Scripts/resume_button.gd
@@ -1,16 +1,11 @@
-extends Control
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var quitButton = $"VBoxContainer/Quit Button"
-	
-	quitButton.pressed.connect(self.on_QuitButton_pressed)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func on_QuitButton_pressed():
-	get_tree().quit()
