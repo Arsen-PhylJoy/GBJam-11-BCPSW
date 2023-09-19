242e1f6fa321144c05f285fd4d0b37216ff4bcbf
clopezochoa <carloslopezochoaaledo@gmail.com>
2023-09-19 11:57:27 +0200

    menu scene theme and intro scene

--- Menu_System/Scripts/intro_scene.gd
+++ Menu_System/Scripts/intro_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$title_animation_test_player.play("title_animation_test")
+	$intro_scene_animation.play("title_animation")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
