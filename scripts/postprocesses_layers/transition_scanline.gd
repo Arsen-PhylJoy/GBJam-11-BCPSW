9ebe24698f971d08b2496da105b2729ab70ce13b
PhylJoy <50301289+PhylJoy@users.noreply.github.com>
2023-09-20 22:46:13 +0300

    Transition shader
    OMG don't look at this code. "It just works" @Tod Howard

--- scripts/postprocesses_layers/transition_scanline.gd
+++ scripts/postprocesses_layers/transition_scanline.gd
@@ -1,14 +1,30 @@
 extends CanvasLayer
-
+##Load this node and prompt start_fade_in or start_fade_out
+##after animation this node deletes itself
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var free_timer: Timer = Timer.new()
-	add_child(free_timer)
-	free_timer.start(8)
-	free_timer.connect("timeout",self.queue_free)
-
+	process_mode = Node.PROCESS_MODE_ALWAYS
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func start_fade_in() -> void:
+	$Fade_In_Out.show()
+	var tween = get_tree().create_tween()
+	get_tree().paused = true
+	$AnimationPlayer.play_backwards("fade_in")
+	await get_tree().create_timer(2).timeout
+	get_tree().paused = false
+	self.queue_free()
+	
+func start_fade_out() -> void:
+	$Fade_In_Out.show()
+	var tween = get_tree().create_tween()
+	get_tree().paused = true
+	$AnimationPlayer.play("fade_in")
+	await get_tree().create_timer(2).timeout
+	get_tree().paused = false
+	self.queue_free()
