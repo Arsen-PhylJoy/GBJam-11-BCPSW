e8a8f896bca30a6a555340fe2408d6327a6793b6
PhylJoy <50301289+PhylJoy@users.noreply.github.com>
2023-09-20 09:54:43 +0300

    Edit pixelation shader and Create scanline shader
    Scanline does'nt work as intended yet
    Delete postprocess_plane because we will apparently use concrete postprocessing layers for concrete postprocesses.

---
+++ _debug/test_levels/test_level_shaders.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _ready() -> void:
+	get_tree().root.size = Vector2(1000,900)
+	get_tree().root.position = Vector2(100,100)
---
+++ scripts/others/transition_scanline.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var free_timer: Timer = Timer.new()
+	add_child(free_timer)
+	free_timer.start(8)
+	free_timer.connect("timeout",self.queue_free)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
