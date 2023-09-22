f5bda7e9f5111eb47ff459adf4093e4cafe39d51
PhylJoy <50301289+PhylJoy@users.noreply.github.com>
2023-09-22 15:51:07 +0300

    Fix bug when game breakes while receiving input meanwhile transition

--- scripts/postprocesses_layers/transition_scanline.gd
+++ scripts/postprocesses_layers/transition_scanline.gd
@@ -5,12 +5,15 @@ signal finished()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	set_process_input(false)
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
+func _exit_tree() -> void:
+		set_process_input(true)
 
 func start_fade_in() -> void:
 	get_tree().paused = true
